db562a9b624deb61ce9a98e4ec9c25ef9eb9e3fd
Matt Driscoll <MDriscoll@esri.com>
2014-06-24 14:28:20 -0700

    default geoform webmap

--- config/defaults.js
+++ config/defaults.js
@@ -3,7 +3,7 @@ define({
     //default web map, default app color theme and more. These values can be overwritten by template configuration settings
     //and url parameters.
     "appid": "",
-    "webmap": "5fd247b0e5d844d99b7b9af36286a535",
+    "webmap": "0c5cb13c4fc54b28bb26a125221ed96f",
     "form_layer": {
         "id": ""
     },
