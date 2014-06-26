f27d529a2edf3752f3d8ac70401c6a0476f71b92
Matt Driscoll <matthew.driscoll@gmail.com>
2014-06-26 13:57:00 -0700

    no message

--- js/edit/offlineOptions.js
+++ js/edit/offlineOptions.js
@@ -3,7 +3,7 @@ Offline.options = {
   checks: {
     image: {
       url: function () {
-        return 'http://esri.github.io/geoform-template-js/images/online-check.png.png?_=' + (Math.floor(Math.random() * 1000000000));
+        return 'http://esri.github.io/geoform-template-js/images/online-check.png?_=' + (Math.floor(Math.random() * 1000000000));
       }
     },
     active: 'image'
