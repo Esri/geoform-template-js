a7574d20ce16db32832b48c3e190b250a6364fe2
Matt Driscoll <MDriscoll@esri.com>
2014-07-25 15:11:52 -0700

    add all themes

--- js/themes.js
+++ js/themes.js
@@ -1,39 +1,99 @@
 define(
-  [{
+    [{
         "id": "bootstrap",
         "name": "Bootstrap (Default)",
         "url": "//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.1.1/css/bootstrap-theme.min.css",
         "thumbnail": "images/themes/default.jpg",
         "refUrl": "http://bootswatch.com/default/"
-  }, {
+    }, {
+        "id": "amelia",
+        "name": "Bootswatch: Amelia",
+        "url": "//cdnjs.cloudflare.com/ajax/libs/bootswatch/3.1.1-1/css/amelia/bootstrap.min.css",
+        "thumbnail": "images/themes/amelia.jpg",
+        "refUrl": "http://bootswatch.com/amelia/"
+    }, {
+        "id": "cerulean",
+        "name": "Bootswatch: Cerulean",
+        "url": "//cdnjs.cloudflare.com/ajax/libs/bootswatch/3.1.1-1/css/cerulean/bootstrap.min.css",
+        "thumbnail": "images/themes/cerulean.jpg",
+        "refUrl": "http://bootswatch.com/cerulean/"
+    }, {
+        "id": "cosmo",
+        "name": "Bootswatch: Cosmo",
+        "url": "//cdnjs.cloudflare.com/ajax/libs/bootswatch/3.1.1-1/css/cosmo/bootstrap.min.css",
+        "thumbnail": "images/themes/cosmo.jpg",
+        "refUrl": "http://bootswatch.com/cosmo/"
+    }, {
         "id": "cyborg",
         "name": "Bootswatch: Cyborg",
         "url": "//cdnjs.cloudflare.com/ajax/libs/bootswatch/3.1.1-1/css/cyborg/bootstrap.min.css",
         "thumbnail": "images/themes/cyborg.jpg",
         "refUrl": "http://bootswatch.com/cyborg/"
-  }, {
-        "id": "cerulian",
-        "name": "Bootswatch: Cerulean",
-        "url": "//cdnjs.cloudflare.com/ajax/libs/bootswatch/3.1.1-1/css/cerulean/bootstrap.min.css",
-        "thumbnail": "images/themes/cerulian.jpg",
-        "refUrl": "http://bootswatch.com/cerulean/"
-  }, {
-        "id": "journal",
-        "name": "Bootswatch: Journal",
-        "url": "//cdnjs.cloudflare.com/ajax/libs/bootswatch/3.1.1-1/css/journal/bootstrap.min.css",
-        "thumbnail": "images/themes/journal.jpg",
-        "refUrl": "http://bootswatch.com/journal/"
-  }, {
+    }, {
         "id": "darkly",
         "name": "Bootswatch: Darkly",
         "url": "//cdnjs.cloudflare.com/ajax/libs/bootswatch/3.1.1-1/css/darkly/bootstrap.min.css",
         "thumbnail": "images/themes/darkly.jpg",
         "refUrl": "http://bootswatch.com/darkly/"
-  }, {
+    }, {
+        "id": "flatly",
+        "name": "Bootswatch: Flatly",
+        "url": "//cdnjs.cloudflare.com/ajax/libs/bootswatch/3.1.1-1/css/flatly/bootstrap.min.css",
+        "thumbnail": "images/themes/flatly.jpg",
+        "refUrl": "http://bootswatch.com/flatly/"
+    }, {
+        "id": "journal",
+        "name": "Bootswatch: Journal",
+        "url": "//cdnjs.cloudflare.com/ajax/libs/bootswatch/3.1.1-1/css/journal/bootstrap.min.css",
+        "thumbnail": "images/themes/journal.jpg",
+        "refUrl": "http://bootswatch.com/journal/"
+    }, {
+        "id": "lumen",
+        "name": "Bootswatch: Lumen",
+        "url": "//cdnjs.cloudflare.com/ajax/libs/bootswatch/3.1.1-1/css/lumen/bootstrap.min.css",
+        "thumbnail": "images/themes/lumen.jpg",
+        "refUrl": "http://bootswatch.com/lumen/"
+    }, {
         "id": "readable",
         "name": "Bootswatch: Readable",
         "url": "//cdnjs.cloudflare.com/ajax/libs/bootswatch/3.1.1-1/css/readable/bootstrap.min.css",
         "thumbnail": "images/themes/readable.jpg",
         "refUrl": "http://bootswatch.com/readable/"
-  }]
+    }, {
+        "id": "simplex",
+        "name": "Bootswatch: Simplex",
+        "url": "//cdnjs.cloudflare.com/ajax/libs/bootswatch/3.1.1-1/css/simplex/bootstrap.min.css",
+        "thumbnail": "images/themes/simplex.jpg",
+        "refUrl": "http://bootswatch.com/simplex/"
+    }, {
+        "id": "slate",
+        "name": "Bootswatch: Slate",
+        "url": "//cdnjs.cloudflare.com/ajax/libs/bootswatch/3.1.1-1/css/slate/bootstrap.min.css",
+        "thumbnail": "images/themes/slate.jpg",
+        "refUrl": "http://bootswatch.com/slate/"
+    }, {
+        "id": "spacelab",
+        "name": "Bootswatch: Spacelab",
+        "url": "//cdnjs.cloudflare.com/ajax/libs/bootswatch/3.1.1-1/css/spacelab/bootstrap.min.css",
+        "thumbnail": "images/themes/spacelab.jpg",
+        "refUrl": "http://bootswatch.com/spacelab/"
+    }, {
+        "id": "superhero",
+        "name": "Bootswatch: Superhero",
+        "url": "//cdnjs.cloudflare.com/ajax/libs/bootswatch/3.1.1-1/css/superhero/bootstrap.min.css",
+        "thumbnail": "images/themes/superhero.jpg",
+        "refUrl": "http://bootswatch.com/superhero/"
+    }, {
+        "id": "united",
+        "name": "Bootswatch: United",
+        "url": "//cdnjs.cloudflare.com/ajax/libs/bootswatch/3.1.1-1/css/united/bootstrap.min.css",
+        "thumbnail": "images/themes/united.jpg",
+        "refUrl": "http://bootswatch.com/united/"
+    }, {
+        "id": "yeti",
+        "name": "Bootswatch: Yeti",
+        "url": "//cdnjs.cloudflare.com/ajax/libs/bootswatch/3.1.1-1/css/yeti/bootstrap.min.css",
+        "thumbnail": "images/themes/yeti.jpg",
+        "refUrl": "http://bootswatch.com/yeti/"
+    }]
 );
