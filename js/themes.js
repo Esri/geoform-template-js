c17da4ac55629b9d4485bee2522f5a1da1acb2cf
Matt Driscoll <MDriscoll@esri.com>
2014-11-05 16:01:26 -0800

    update bootswatch

--- js/themes.js
+++ js/themes.js
@@ -8,104 +8,104 @@ define(
     }, {
         "id": "amelia",
         "name": "Amelia",
-        "url": "js/vendor/bootswatch-3.2.0-4/amelia/bootstrap.min.css",
-        "thumbnail": "js/vendor/bootswatch-3.2.0-4/amelia/thumbnail.png",
+        "url": "js/vendor/bootswatch-3.3.0/amelia/bootstrap.min.css",
+        "thumbnail": "js/vendor/bootswatch-3.3.0/amelia/thumbnail.png",
         "refUrl": "http://bootswatch.com/amelia/"
     }, {
         "id": "cerulean",
         "name": "Cerulean",
-        "url": "js/vendor/bootswatch-3.2.0-4/cerulean/bootstrap.min.css",
-        "thumbnail": "js/vendor/bootswatch-3.2.0-4/cerulean/thumbnail.png",
+        "url": "js/vendor/bootswatch-3.3.0/cerulean/bootstrap.min.css",
+        "thumbnail": "js/vendor/bootswatch-3.3.0/cerulean/thumbnail.png",
         "refUrl": "http://bootswatch.com/cerulean/"
     }, {
         "id": "cosmo",
         "name": "Cosmo",
-        "url": "js/vendor/bootswatch-3.2.0-4/cosmo/bootstrap.min.css",
-        "thumbnail": "js/vendor/bootswatch-3.2.0-4/cosmo/thumbnail.png",
+        "url": "js/vendor/bootswatch-3.3.0/cosmo/bootstrap.min.css",
+        "thumbnail": "js/vendor/bootswatch-3.3.0/cosmo/thumbnail.png",
         "refUrl": "http://bootswatch.com/cosmo/"
     }, {
         "id": "cyborg",
         "name": "Cyborg",
-        "url": "js/vendor/bootswatch-3.2.0-4/cyborg/bootstrap.min.css",
-        "thumbnail": "js/vendor/bootswatch-3.2.0-4/cyborg/thumbnail.png",
+        "url": "js/vendor/bootswatch-3.3.0/cyborg/bootstrap.min.css",
+        "thumbnail": "js/vendor/bootswatch-3.3.0/cyborg/thumbnail.png",
         "refUrl": "http://bootswatch.com/cyborg/"
     }, {
         "id": "darkly",
         "name": "Darkly",
-        "url": "js/vendor/bootswatch-3.2.0-4/darkly/bootstrap.min.css",
-        "thumbnail": "js/vendor/bootswatch-3.2.0-4/darkly/thumbnail.png",
+        "url": "js/vendor/bootswatch-3.3.0/darkly/bootstrap.min.css",
+        "thumbnail": "js/vendor/bootswatch-3.3.0/darkly/thumbnail.png",
         "refUrl": "http://bootswatch.com/darkly/"
     }, {
         "id": "flatly",
         "name": "Flatly",
-        "url": "js/vendor/bootswatch-3.2.0-4/flatly/bootstrap.min.css",
-        "thumbnail": "js/vendor/bootswatch-3.2.0-4/flatly/thumbnail.png",
+        "url": "js/vendor/bootswatch-3.3.0/flatly/bootstrap.min.css",
+        "thumbnail": "js/vendor/bootswatch-3.3.0/flatly/thumbnail.png",
         "refUrl": "http://bootswatch.com/flatly/"
     }, {
         "id": "journal",
         "name": "Journal",
-        "url": "js/vendor/bootswatch-3.2.0-4/journal/bootstrap.min.css",
-        "thumbnail": "js/vendor/bootswatch-3.2.0-4/journal/thumbnail.png",
+        "url": "js/vendor/bootswatch-3.3.0/journal/bootstrap.min.css",
+        "thumbnail": "js/vendor/bootswatch-3.3.0/journal/thumbnail.png",
         "refUrl": "http://bootswatch.com/journal/"
     }, {
         "id": "lumen",
         "name": "Lumen",
-        "url": "js/vendor/bootswatch-3.2.0-4/lumen/bootstrap.min.css",
-        "thumbnail": "js/vendor/bootswatch-3.2.0-4/lumen/thumbnail.png",
+        "url": "js/vendor/bootswatch-3.3.0/lumen/bootstrap.min.css",
+        "thumbnail": "js/vendor/bootswatch-3.3.0/lumen/thumbnail.png",
         "refUrl": "http://bootswatch.com/lumen/"
     }, {
         "id": "paper",
         "name": "Paper",
-        "url": "js/vendor/bootswatch-3.2.0-4/paper/bootstrap.min.css",
-        "thumbnail": "js/vendor/bootswatch-3.2.0-4/paper/thumbnail.png",
+        "url": "js/vendor/bootswatch-3.3.0/paper/bootstrap.min.css",
+        "thumbnail": "js/vendor/bootswatch-3.3.0/paper/thumbnail.png",
         "refUrl": "http://bootswatch.com/paper/"
     }, {
         "id": "readable",
         "name": "Readable",
-        "url": "js/vendor/bootswatch-3.2.0-4/readable/bootstrap.min.css",
-        "thumbnail": "js/vendor/bootswatch-3.2.0-4/readable/thumbnail.png",
+        "url": "js/vendor/bootswatch-3.3.0/readable/bootstrap.min.css",
+        "thumbnail": "js/vendor/bootswatch-3.3.0/readable/thumbnail.png",
         "refUrl": "http://bootswatch.com/readable/"
     }, {
         "id": "sandstone",
         "name": "Sandstone",
-        "url": "js/vendor/bootswatch-3.2.0-4/sandstone/bootstrap.min.css",
-        "thumbnail": "js/vendor/bootswatch-3.2.0-4/sandstone/thumbnail.png",
+        "url": "js/vendor/bootswatch-3.3.0/sandstone/bootstrap.min.css",
+        "thumbnail": "js/vendor/bootswatch-3.3.0/sandstone/thumbnail.png",
         "refUrl": "http://bootswatch.com/sandstone/"
     }, {
         "id": "simplex",
         "name": "Simplex",
-        "url": "js/vendor/bootswatch-3.2.0-4/simplex/bootstrap.min.css",
-        "thumbnail": "js/vendor/bootswatch-3.2.0-4/simplex/thumbnail.png",
+        "url": "js/vendor/bootswatch-3.3.0/simplex/bootstrap.min.css",
+        "thumbnail": "js/vendor/bootswatch-3.3.0/simplex/thumbnail.png",
         "refUrl": "http://bootswatch.com/simplex/"
     }, {
         "id": "slate",
         "name": "Slate",
-        "url": "js/vendor/bootswatch-3.2.0-4/slate/bootstrap.min.css",
-        "thumbnail": "js/vendor/bootswatch-3.2.0-4/slate/thumbnail.png",
+        "url": "js/vendor/bootswatch-3.3.0/slate/bootstrap.min.css",
+        "thumbnail": "js/vendor/bootswatch-3.3.0/slate/thumbnail.png",
         "refUrl": "http://bootswatch.com/slate/"
     }, {
         "id": "spacelab",
         "name": "Spacelab",
-        "url": "js/vendor/bootswatch-3.2.0-4/spacelab/bootstrap.min.css",
-        "thumbnail": "js/vendor/bootswatch-3.2.0-4/spacelab/thumbnail.png",
+        "url": "js/vendor/bootswatch-3.3.0/spacelab/bootstrap.min.css",
+        "thumbnail": "js/vendor/bootswatch-3.3.0/spacelab/thumbnail.png",
         "refUrl": "http://bootswatch.com/spacelab/"
     }, {
         "id": "superhero",
         "name": "Superhero",
-        "url": "js/vendor/bootswatch-3.2.0-4/superhero/bootstrap.min.css",
-        "thumbnail": "js/vendor/bootswatch-3.2.0-4/superhero/thumbnail.png",
+        "url": "js/vendor/bootswatch-3.3.0/superhero/bootstrap.min.css",
+        "thumbnail": "js/vendor/bootswatch-3.3.0/superhero/thumbnail.png",
         "refUrl": "http://bootswatch.com/superhero/"
     }, {
         "id": "united",
         "name": "United",
-        "url": "js/vendor/bootswatch-3.2.0-4/united/bootstrap.min.css",
-        "thumbnail": "js/vendor/bootswatch-3.2.0-4/united/thumbnail.png",
+        "url": "js/vendor/bootswatch-3.3.0/united/bootstrap.min.css",
+        "thumbnail": "js/vendor/bootswatch-3.3.0/united/thumbnail.png",
         "refUrl": "http://bootswatch.com/united/"
     }, {
         "id": "yeti",
         "name": "Yeti",
-        "url": "js/vendor/bootswatch-3.2.0-4/yeti/bootstrap.min.css",
-        "thumbnail": "js/vendor/bootswatch-3.2.0-4/yeti/thumbnail.png",
+        "url": "js/vendor/bootswatch-3.3.0/yeti/bootstrap.min.css",
+        "thumbnail": "js/vendor/bootswatch-3.3.0/yeti/thumbnail.png",
         "refUrl": "http://bootswatch.com/yeti/"
     }]
 );
